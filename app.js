ed0a3554a202ed8c01cf4b3c57a2b1b73151567e
dinojKumarV <vdinojkumar@gmail.com>
2023-02-11 16:38:17 +0530

    modified the previous code and add some new thing to it

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const names= require('./4namesmodule')
 
 const sayhi=require('./3modules')
 
+require('./sums')
+
 sayhi(names.ani)
 sayhi(names.dinoj)
---
+++ sums.js
@@ -0,0 +1,8 @@
+const n1=8
+const n2 = 109876
+
+function add(){
+    console.log(`the sum is ${n1+n2}`)
+}
+
+add()
