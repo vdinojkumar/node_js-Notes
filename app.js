3d7e156b2e89e0fe2e203734a4f90c9593570ee0
dinojKumarV <vdinojkumar@gmail.com>
2023-02-12 20:13:22 +0530

    modified the previous code and add some new thing to it

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const {readFileSync , writeFileSync} = require('fs')
 
-const fs =reqire('fs')
-fs.readFileSync
+
+const first= readFileSync('./content/first.txt','utf8')
+const second= readFileSync('./content/second.txt','utf8')
+
+//console.log(first,second)
+
+writeFileSync('./content/result-sync.txt',
+`here is the result :${first},${second}`)
+
+
