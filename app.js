b1905c89c0c31663df6d076103c8604a1e335d21
dinojKumarV <vdinojkumar@gmail.com>
2023-02-14 20:36:07 +0530

    nodeJsNotes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//npm -> node package manager 
+//npm - global command , comes with node 
+//npm -- version
+
+//local dependency -- use it only in this particular project
+// npm i <pacakge name>
+
+//global dependency - use it in any project
+// npm install -g <packageName>
+//sudo npm install -g <packageName> 
+
+
+//package.json - manifest files ( stores important info about projects / packages )
+//manual approach (create package.json in root , creates propertirsn etc)
+// npm init(setp by step , press enter to skip)
+//npm inti -y(everthing default)
+
+
