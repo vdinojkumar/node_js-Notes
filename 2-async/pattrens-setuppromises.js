7039a3afb729b34fb9bb3ee8276859f6afdcf3e3
vdinojkumar <dkvd360@gmail.com>
2023-02-22 19:41:12 +0530

    Async pattren -setup promise

---
+++ 2-async/pattrens-setuppromises.js
@@ -0,0 +1,17 @@
+const {readFile}=require('fs')
+
+
+const getText=(path)=>{
+    return new Promise((resolve, reject)=>{
+        readFile(path,'utf8',(err,data)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
+}
+
+
+getText('./content/first.txt').then((result)=>console.log(result)).catch((err)=> console.log(err))
